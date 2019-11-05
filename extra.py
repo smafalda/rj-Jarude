043e21893159cbcee48f773f3636029451c902c6
Salomão Mafalda <56613548+smafalda@users.noreply.github.com>
2019-11-05 16:12:34 -0500

    Add files via upload

--- extra.py
+++ extra.py
@@ -2,28 +2,31 @@ import requests
 import boxsdk
 from boxsdk import DevelopmentClient
 import xlrd
-client = DevelopmentClient()
-user = client.user().get()
 from boxsdk import OAuth2, Client
 import pickle
 import os
 import json
-import base64
+client = DevelopmentClient()
+user = client.user().get()
 #dYWuBYWplZ1kPZCxDGkR6yWzEoaEt5dT
 
-#items_iter = client.folder(folder_id='31436627801').get_items(limit=100, offset=0, fields='id', 'name' , 'type')
+entrada = sys.argv[1:]
+workbook = xldr.open_workbook(entrada[0])
+Folha1 = workbook.sheet_by_index(0)
 ABSOLUTE_PATH = os.getcwd()
 os.mkdir(ABSOLUTE_PATH+'\\Resultados')
-items = client.search().query(query='"-ARC97-"', file_extensions=['pdf'], content_types='name', ancestor_folder_ids=31436627801,type='file')
-data_temp =''
-idanterior=''
-for item in items:
-    if item.created_at < data_temp:
-        print("Arquivo recente existente")
-    else:
-        idRecente=item.id
-        data_temp = item.created_at
-        idanterior= item.id
+for i in range(1, Folha1):
+    nomeArq= Folha1.cell_value(i,0)
+    items = client.search().query(query='"'+nomeArq+'"', file_extensions=['pdf'], content_types='name', ancestor_folder_ids=31436627801,type='file')
+    data_temp =''
+    idanterior=''
+    for item in items:
+        if item.created_at < data_temp:
+            print("Arquivo recente existente")
+        else:
+            idRecente=item.id
+            data_temp = item.created_at
+            idanterior= item.id
     box_file = client.file(file_id=idRecente).get()
     output_file = open(ABSOLUTE_PATH+'\\Resultados\\'+ box_file.name, 'wb')
     box_file.download_to(output_file)
