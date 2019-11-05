d3fcdc7aaed2444c1714e71cdd71986cd7a15c26
Salomão Mafalda <56613548+smafalda@users.noreply.github.com>
2019-11-05 17:18:54 -0500

    Add files via upload

--- extra.py
+++ extra.py
@@ -6,27 +6,32 @@ from boxsdk import OAuth2, Client
 import pickle
 import os
 import json
+import sys
 client = DevelopmentClient()
 user = client.user().get()
-#dYWuBYWplZ1kPZCxDGkR6yWzEoaEt5dT
+#mapwYH46MYdvRR47j3YmtxhN1V7tdbTA
 
 entrada = sys.argv[1:]
-workbook = xldr.open_workbook(entrada[0])
+workbook = xlrd.open_workbook(entrada[0])
 Folha1 = workbook.sheet_by_index(0)
 ABSOLUTE_PATH = os.getcwd()
 os.mkdir(ABSOLUTE_PATH+'\\Resultados')
-for i in range(1, Folha1):
+for i in range(1, Folha1.nrows):
     nomeArq= Folha1.cell_value(i,0)
+    nomeArq.replace(' ', '')
     items = client.search().query(query='"'+nomeArq+'"', file_extensions=['pdf'], content_types='name', ancestor_folder_ids=31436627801,type='file')
     data_temp =''
     idanterior=''
+    idRecente=''
     for item in items:
-        if item.created_at < data_temp:
-            print("Arquivo recente existente")
-        else:
-            idRecente=item.id
-            data_temp = item.created_at
-            idanterior= item.id
-    box_file = client.file(file_id=idRecente).get()
-    output_file = open(ABSOLUTE_PATH+'\\Resultados\\'+ box_file.name, 'wb')
-    box_file.download_to(output_file)
+        if ((nomeArq+'-' in item.name)or (nomeArq+'-' in item.name) ):
+            if item.created_at < data_temp:
+                print("Arquivo recente existente")
+            else:
+                idRecente=item.id
+                data_temp = item.created_at
+                idanterior= item.id
+    if idRecente!='':
+        box_file = client.file(file_id=idRecente).get()
+        output_file = open(ABSOLUTE_PATH+'\\Resultados\\'+ box_file.name, 'wb')
+        box_file.download_to(output_file)
--- extraNoExcel.py
+++ extraNoExcel.py
@@ -13,19 +13,4 @@ user = client.user().get()
 #entrada = sys.argv[1:]
 #workbook = xldr.open_workbook(entrada[0])
 #Folha1 = workbook.sheet_by_index(0)
-ABSOLUTE_PATH = os.getcwd()
-os.mkdir(ABSOLUTE_PATH+'\\Resultados')
-#nomeArq= Folha1.cell_value(i,0)
-items = client.search().query(query='"-ARC97-"', file_extensions=['pdf'], content_types='name', ancestor_folder_ids=31436627801,type='file')
-data_temp =''
-idanterior=''
-for item in items:
-    if item.created_at < data_temp:
-        print("Arquivo recente existente")
-    else:
-        idRecente=item.id
-        data_temp = item.created_at
-        idanterior= item.id
-box_file = client.file(file_id=idRecente).get()
-output_file = open(ABSOLUTE_PATH+'\\Resultados\\'+ box_file.name, 'wb')
-box_file.download_to(output_file)
+print(" ARC6 ".replace(' ', ''))
